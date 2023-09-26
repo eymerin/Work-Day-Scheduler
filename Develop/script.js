5c6ce2efe24ce3cfb7305e32e4d7a53eb86fb3ce
eymerin <garrett.bryce.young@gmail.com>
2023-09-25 19:08:21 -0600

    updated js, added css classes

--- Develop/script.js
+++ Develop/script.js
@@ -3,24 +3,21 @@
 // in the html.
 
 $(function () {
-
-  // This code should use the id in the containing time-block as a key to save the user input in local storage.
-  // How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? 
   
+  // gets current time and date and displays it
+  var currentTime = dayjs().format("dddd, MMMM D h:mm A");
+  var timeDate = $(`<div class="bold date">${currentTime}</div>`);
+  $("#head").append(timeDate);
 
-
-  // time block creation function - use for loop to create a timeblock ex-"hour-9" html element inside
-  // container div for every hour 9-18 containing columns for hour, text area, save button compare each
-  // time block time value to current time and add classes for past, present, and future to timeblock object
   // Create the div element
   $(document).ready(function() {
 
+    // build calendar
     for(let time of [8,9,10,11,12,13,14,15,16,17,18,19]){
-      console.log(time);
+      
+      // declare variables
       var currentHour12 = parseInt(dayjs().format("h"));
-      console.log(currentHour12);
       var currentHour24 = parseInt(dayjs().format("H"));
-      console.log(currentHour24);
       var timeClass;
       var amPM = " AM";
 
@@ -39,9 +36,9 @@ $(function () {
       if (time>12){
         time -= 12;
         amPM = " PM"
-
       }
 
+      // Build timeboxes
       var divElement = $(`<div id="hour-${time}" class="row time-block ${timeClass}"></div>`);
       $("#container").append(divElement);
       $(`#hour-${time}`).append(`<div class= 'col-2 col-md-1 hour text-center py-3'>${time+amPM}</div>`);
@@ -49,6 +46,9 @@ $(function () {
       $(`#hour-${time}`).append("<button class='btn saveBtn col-2 col-md-1' aria-label='save'> <i class='fas fa-save' aria-hidden='true'></i> </button>");
       
       // read from local storage to populate textareas
+
+      //let #hour-${time}.textarea = localStorage.getItem(#hour-${time});
+
       // TODO: Add code to get any user input that was saved in localStorage and set
       // the values of the corresponding textarea elements. HINT: How can the id
       // attribute of each time-block be used to do this?
@@ -68,15 +68,12 @@ $(function () {
 
   // save button - on click, set timeblock hour as key and entered text (with appended current time) 
   // as value to local storage 
-  // localStorage.setItem(key, value);
+  localStorage.setItem(key, value);
   //const button = document.getElementById('hour-10');
 
-  //button.addEventListener('click', function() {
-  //  const currentDate = dayjs();
-  //  console.log(currentDate.format('YYYY-MM-DD HH:mm:ss'));
-  //  var formattedDate = currentDate.format("h:mm A");
-  //  console.log(formattedDate);
-  //});
+  button.addEventListener('click', function() {
+    
+  });
   
   // TODO: Add code to display the current date in the header of the page.
 
