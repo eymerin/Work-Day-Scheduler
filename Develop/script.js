a60be176ac16babe00d04667063306ab8848661c
eymerin <garrett.bryce.young@gmail.com>
2023-09-28 14:35:50 -0600

    added final changes to js, html, and css

--- Develop/script.js
+++ Develop/script.js
@@ -16,7 +16,6 @@ $(function () {
     for(let time of [8,9,10,11,12,13,14,15,16,17,18,19]){
       
       // declare variables
-      var currentHour12 = parseInt(dayjs().format("h"));
       var currentHour24 = parseInt(dayjs().format("H"));
       var timeClass;
       var amPM = " AM";
@@ -43,10 +42,10 @@ $(function () {
       $("#container").append(divElement);
       $(`#hour-${time}`).append(`<div class= 'col-2 col-md-1 hour text-center py-3'>${time+amPM}</div>`);
       $(`#hour-${time}`).append("<textarea class= 'col-8 col-md-10 description' rows='3'> </textarea>");
-      $(`#hour-${time}`).append("<button class='btn saveBtn col-2 col-md-1' aria-label='save'> <i class='fas fa-save' aria-hidden='true'></i> </button>");
+      $(`#hour-${time}`).append(`<button id='btn${time}' ' class='btn saveBtn col-2 col-md-1' aria-label='save'> <i class='fas fa-save' aria-hidden='true'></i> </button>`);
       
       // read from local storage to populate textareas
-
+      //$(`#hour-${time}.description`).val(localStorage.getItem(`hour-${time}`))
       //let #hour-${time}.textarea = localStorage.getItem(#hour-${time});
 
       // TODO: Add code to get any user input that was saved in localStorage and set
@@ -55,12 +54,38 @@ $(function () {
       // local storage search function - call local storage search for each timeblock to find hour and text values, then 
       // json parse last entry's time(key) and text(value)
       // let `#hour-${time}.textarea` = localStorage.getItem(#hour-${time});
-    }
-  
-  
-  
 
-    // Append the div element to the body or any other element
+    }
+    
+    var btn8 = $('#btn8');
+
+    function handleClick(event) {
+      var btnId = event.currentTarget.id;
+      console.log("Clicked button ID:", btnId);
+    }
+
+    $(".saveBtn").on("click", function(){
+      var text= $(this).siblings(".description").val()
+      var timeStamp= $(this).parent().attr("id")
+      localStorage.setItem(timeStamp, text)
+      console.log(timeStamp, text)
+    })
+
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"))
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"))
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"))
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"))
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"))
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"))
+    $("#hour-6 .description").val(localStorage.getItem("hour-6"))
+    $("#hour-7 .description").val(localStorage.getItem("hour-7"))
+
+
+
 
   }
   );
@@ -68,13 +93,11 @@ $(function () {
 
   // save button - on click, set timeblock hour as key and entered text (with appended current time) 
   // as value to local storage 
-  localStorage.setItem(key, value);
-  //const button = document.getElementById('hour-10');
 
-  button.addEventListener('click', function() {
-    
-  });
-  
-  // TODO: Add code to display the current date in the header of the page.
+
+
+
+
+
 
 });
